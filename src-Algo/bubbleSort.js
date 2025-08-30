e4a198b4f75eae6393a68c6ad032896ecbc3b933
AlokSinghal328 <aloksinghal328@gmail.com>
2025-08-31 00:33:26 +0530

    solving the issue with the start button

--- src-Algo/bubbleSort.js
+++ src-Algo/bubbleSort.js
@@ -12,7 +12,6 @@ function init() {
   document.querySelector(".input-space").addEventListener("input", (e) => {
     if (!isStarted) {
       arraySize = parseInt(e.target.value);
-    } else {
     }
   });
 
@@ -53,7 +52,7 @@ async function bubbleSort() {
   isStarted = true;
   for (let i = 0; i < array.length - 1; i++) {
     for (let j = 0; j < array.length - i - 1; j++) {
-      if(isStoped) return;
+      if (isStoped) return;
       while (isPaused) await new Promise((r) => setTimeout(r, 200));
 
       if (array[j] > array[j + 1]) {
